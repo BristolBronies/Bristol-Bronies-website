3a1eade6910c5a142a5d16c6f81b299fb8deb519
Grey Hargreaves <shaun.hargreaves@gmail.com>
2015-10-24 17:21:16 +0100

    Staff page changes

--- page-staff.php
+++ page-staff.php
@@ -6,30 +6,30 @@
 	get_template_part('partials/global/header');
 ?>
 
+<?php 
+	if(have_posts()):
+		while(have_posts()): the_post(); 
+?>
+
 	<main class="body" id="content" role="main">
-		<?php 
-			while ( have_posts() ) : the_post(); 
-		?>
-		<section class="template-staff__intro">
-			<div class="layout">
-				<div class="content">
+		<div class="layout">
+			<article class="article">
+				<header class="article__header">
+					<h1 class="article__title"><?php the_title(); ?></h1>
+				</header>
+				<div class="content article__body">
 					<?php the_content(); ?>
 				</div>
-			</div>
-		</section>
-		<?php 
-			endwhile; 
-		?>
-		<?php 
-			$staff = new WP_Query('post_type=meet_runner&posts_per_page=-1&orderby=title&order=ASC');
-			if($staff->have_posts()):
-		?>
-		<section class="template-about__staff">
-			<?php 
-				while($staff->have_posts()): $staff->the_post();
-					if(get_field("runner_staff") == true): 
-			?>
-						<article class="staff template-about__staff__item">
+				<?php 
+					$staff = new WP_Query('post_type=meet_runner&posts_per_page=-1&orderby=title&order=ASC');
+					if($staff->have_posts()):
+				?>
+				<div class="template-staff__list">
+					<?php 
+						while($staff->have_posts()): $staff->the_post();
+							if(get_field("runner_staff") == true): 
+					?>
+						<section class="staff template-staff__item">
 							<div class="staff__banner<?php if($banner = bb_profile_banner($runner)): ?> staff__banner--custom<?php endif; ?>" style="background-color: <?php echo bb_generate_colour(get_the_title()); ?>; <?php if($banner = bb_profile_banner($runner)): ?>background-image: url(<?php echo $banner; ?>);<?php endif; ?>"></div>
 							<img class="staff__avatar" alt="" src="<?php echo bb_profile_avatar($runner); ?>">
 							<div class="staff__body">
@@ -56,17 +56,24 @@
 									</ul>
 								</div>
 							</div>
-						</article>
-			<?php 
-					endif;
-				endwhile;
+						</section>
+					<?php 
+							endif;
+						endwhile;
+					?>
+				</div>
+			</article>
+			<?php
+				endif; 
+				wp_reset_postdata();
 			?>
-		</section>
-		<?php
-			endif; 
-			wp_reset_postdata();
-		?>
+		</div>
 	</main>
+
+<?php
+		endwhile;
+	endif; 
+?>
 
 <?php
 	get_template_part('partials/global/footer');
